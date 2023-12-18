8424ba75708e90e0b69ebb671be833b75de981a1
Spudnick9 <152810331+Spudnick9@users.noreply.github.com>
2023-12-17 19:59:26 -0500

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,45 +1,111 @@
+#Thomas Scherrer
+#Registration Form
+#Uses tkinter, messagebox, and a separate file to store the login information of each person that creates and account. 
+
+
+
+
 import tkinter as tk
 from tkinter import messagebox
 
-window = tk.Tk()
-window.title("Final")
-window.geometry("500x500")
+#creating window object
+form = tk.Tk()
+form.title("Restration Form")
 
+ 
 
+def openwindow():
+    def signin():
+        openfile = open("details.txt", "r")
+        totallines = openfile.readlines()
+        firstname = new_ftext.get()
+        email = new_etext.get()
+        
+        for i in totallines:
+            eachline = i.split("|")
+            thefirstname = eachline[0].strip("\n")
+            thelastname = eachline[1].strip("\n")
+            theemail = eachline[2].strip("\n")
+            checker = False
+            if firstname == thefirstname and email == theemail:
+                checker = True
+                break
 
-def login():
-    username = tk.Label(text = 'Enter username: ', font=("Roboto 11"))
-    password = tk.Label(text = 'Enter password: ', font=("Roboto 11"))
-    usernameEntry = tk.Entry(font=("Roboto 11"))
-    passwordEntry = tk.Entry(font=("Roboto 11"))
-    login_button = tk.Button(text = "Login", command = _login_btn_clicked)
-    username.grid(column = 0, row = 1)
-    password.grid(column = 0, row = 2)
-    usernameEntry.grid(column = 1, row = 1)
-    passwordEntry.grid(column = 1, row = 2)
-    login_button.grid(column = 1, row = 3)
-
-def _login_btn_clicked():
-        usernameEntry = tk.Entry(font=("Roboto 11"))
-        passwordEntry = tk.Entry(font=("Roboto 11"))
-        username = usernameEntry.get()
-        password = passwordEntry.get()
-        if username == "username" and password == "password":
-            messagebox.showinfo("Welcome")
+        if checker == True:
+            messagebox.showinfo("Information", "Login Success!")
         else:
-            messagebox.showinfo("Error")
-            
-login()
+            messagebox.showerror("Failed", "Login Failed")
+
+
+
+    window = tk.Toplevel()
+    window.title("Login Form")
+    newframe = tk.Frame(window)
+    newframe.grid(padx=10, pady=10)
+    new_label = tk.Label(newframe, text = "Sign In")
+    new_label.grid(row=0, column= 0, columnspan= 2, pady=5, sticky="we")
+
+    new_flabel = tk.Label(newframe, text = "First name:")
+    new_flabel.grid(row = 1, column= 0,pady=5, sticky="w")
+    new_elabel = tk.Label(newframe, text = "Email:")
+    new_elabel.grid(row = 2, column= 0,pady=5, sticky="w")
+    new_ftext = tk.Entry(newframe)
+    new_ftext.grid(row = 1, column= 1,pady=5)
+    new_etext = tk.Entry(newframe)
+    new_etext.grid(row = 2, column= 1,pady=5)
+    lbutton = tk.Button(newframe, text = "Sign In", command = signin)
+    lbutton.grid(row = 3, column= 0, columnspan= 2,pady=5)
+
+
+def register():
+    detail_file = open("details.txt", "w")
+    detail_file.close()
+    new_detail_file = open("details.txt", "a")
+
+    fname = ftext.get()
+    lname = ltext.get()
+    email = etext.get()
+  
+
+    if len(fname) > 1:
+        if len(lname) > 1:
+            if len(email) > 1:
+                if "@" in email and "." in email:
+                    new_detail_file.write(fname + "|" + lname + "|" + email + "\n")
+                    new_detail_file.close()
+                    messagebox.showinfo("Congratulations","Registration Successful!!")
+                else:
+                    messagebox.showerror("Error","Please enter a valid email")       
+            else:
+                messagebox.showerror("Error","Please do not leave email field blank")
+        else:
+            messagebox.showerror("Error","Please do not leave last name field blank")
+    else:
+        messagebox.showerror("Error","Please do not leave first name field blank")
 
 
 
 
 
+frame = tk.Frame(form)
+frame.grid(padx=10, pady=10)
+tlabel = tk.Label(frame, text = "Registration Form")
+tlabel.grid(row=0, column= 0, columnspan= 2, pady=5, sticky="we")
+flabel = tk.Label(frame, text = "Enter first name:")
+flabel.grid(row = 1, column= 0,pady=5, sticky="w")
+llabel = tk.Label(frame, text = "Enter Last name:")
+llabel.grid(row = 2, column= 0,pady=5, sticky="w")
+elabel = tk.Label(frame, text = "Enter email:")
+elabel.grid(row = 3, column= 0,pady=5, sticky="w")
+ftext = tk.Entry(frame)
+ftext.grid(row = 1, column= 1,pady=5)
+ltext = tk.Entry(frame)
+ltext.grid(row = 2, column= 1,pady=5)
+etext = tk.Entry(frame)
+etext.grid(row = 3, column= 1,pady=5)
+rbutton = tk.Button(frame, text = "Register", command=register)
+rbutton.grid(row = 4, column= 0, columnspan= 2,pady=5)
 
-
-
-
-
-
-
-window.mainloop()
+sbutton = tk.Button(frame, text = "Already registered? click this button to sign in", command=openwindow)
+sbutton.grid(row = 5, column= 0, columnspan= 2,pady=5)
+form.mainloop()
